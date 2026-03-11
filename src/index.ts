a21b27e9b3c3b5d6556d687cba4cf5d5f72f3c1d
albaker-evri-inno-lab <148968978+albaker-evri-inno-lab@users.noreply.github.com>
2026-03-11 17:17:40 +0000

    added default

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { setupExpressRoutes } from './webview';
 import { handleToolCall } from './tools';
 
 const PACKAGE_NAME = process.env.PACKAGE_NAME || 'com.evri.innovation.barcodescanner';
-const MENTRAOS_API_KEY = process.env.MENTRAOS_API_KEY ?? (() => { throw new Error('MENTRAOS_API_KEY is not set in .env file'); })();
+const MENTRAOS_API_KEY = process.env.MENTRAOS_API_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsYXJpY2Jha2VyQGdtYWlsLmNvbSIsInR5cGUiOiJjbGkiLCJrZXlJZCI6IjQxYmIwNmFiLWFhNDUtNDhhNC1iODkxLWU1MjFhN2YzZjk2MyIsIm5hbWUiOiJ3b3JrIG1hY2Jvb2sgcHJvIiwiaWF0IjoxNzczMjI4NDU2fQ.ix9fMRZGS2uVG-EJyCS5fo1Rh7m-VsnKz562M0DOsiI';
 const PORT = parseInt(process.env.PORT || '3000');
 
 class ExampleMentraOSApp extends AppServer {
